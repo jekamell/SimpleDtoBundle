b7dc45937c4977a13788d156eaf54ecbc9bae118
Evgeniy Malykhin <55jeka@gmail.com>
2016-07-25 23:35:52 +0300

    fix configuration

--- DependencyInjection/Configuration.php
+++ DependencyInjection/Configuration.php
@@ -6,9 +6,8 @@ use Symfony\Component\Config\Definition\Builder\TreeBuilder;
 use Symfony\Component\Config\Definition\ConfigurationInterface;
 
 /**
- * This is the class that validates and merges configuration from your app/config files.
- *
- * To learn more see {@link http://symfony.com/doc/current/cookbook/bundles/configuration.html}
+ * Class Configuration
+ * @package Mell\Bundle\SimpleDtoBundle\DependencyInjection
  */
 class Configuration implements ConfigurationInterface
 {
@@ -18,7 +17,7 @@ class Configuration implements ConfigurationInterface
     public function getConfigTreeBuilder()
     {
         $treeBuilder = new TreeBuilder();
-        $rootNode = $treeBuilder->root('mell_rest_api');
+        $rootNode = $treeBuilder->root('simple_dto');
 
         $rootNode
             ->children()
