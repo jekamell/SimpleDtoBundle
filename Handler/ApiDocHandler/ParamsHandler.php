5b296f2ebc5ed86adee990b8a04524e2db2e8594
Evgeniy Malykhin <e.malykhin@richmedia.systems>
2016-07-28 16:00:38 +0300

    auto add _fields and _expands param to get/post/put methods

---
+++ Handler/ApiDocHandler/ParamsHandler.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace Mell\Bundle\SimpleDtoBundle\Handler\ApiDocHandler;
+
+use Mell\Bundle\SimpleDtoBundle\Services\RequestManager\RequestManagerConfigurator;
+use Nelmio\ApiDocBundle\Annotation\ApiDoc;
+use Nelmio\ApiDocBundle\Extractor\HandlerInterface;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Route;
+
+class ParamsHandler implements HandlerInterface
+{
+    /** @var RequestManagerConfigurator */
+    protected $requestManagerConfigurator;
+
+    /**
+     * ParamsHandler constructor.
+     * @param RequestManagerConfigurator $requestManagerConfigurator
+     */
+    public function __construct(RequestManagerConfigurator $requestManagerConfigurator)
+    {
+        $this->requestManagerConfigurator = $requestManagerConfigurator;
+    }
+
+    /**
+     * Parse route parameters in order to populate ApiDoc.
+     *
+     * @param ApiDoc $annotation
+     * @param array $annotations
+     * @param Route $route
+     * @param \ReflectionMethod $method
+     */
+    public function handle(ApiDoc $annotation, array $annotations, Route $route, \ReflectionMethod $method)
+    {
+        if (in_array(Request::METHOD_DELETE, $route->getMethods())) {
+            return;
+        }
+
+        $annotation->addParameter($this->requestManagerConfigurator->getFieldsParam(), $this->getFieldsParams());
+        $annotation->addParameter($this->requestManagerConfigurator->getExpandsParam(), $this->getExpandsParams());
+    }
+
+    /**
+     * @return array
+     */
+    private function getFieldsParams()
+    {
+        return [
+            'dataType' => 'string',
+            'required' => false,
+            'description' => 'Required fields',
+        ];
+    }
+
+    /**
+     * @return array
+     */
+    private function getExpandsParams()
+    {
+        return [
+            'dataType' => 'string',
+            'required' => false,
+            'description' => 'Required expands',
+        ];
+    }
+}
