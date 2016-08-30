cc528f44bab46702c017d0f949b1d3b9ac3c5698
Evgeniy Malykhin <e.malykhin@richmedia.systems>
2016-08-30 12:50:37 +0300

    fix expands listener (was not working on dto collection)

--- EventListener/DtoExpandsListener.php
+++ EventListener/DtoExpandsListener.php
@@ -5,6 +5,7 @@ namespace Mell\Bundle\SimpleDtoBundle\EventListener;
 use Mell\Bundle\SimpleDtoBundle\Event\ApiEvent;
 use Mell\Bundle\SimpleDtoBundle\Helpers\DtoHelper;
 use Mell\Bundle\SimpleDtoBundle\Model\Dto;
+use Mell\Bundle\SimpleDtoBundle\Model\DtoCollectionInterface;
 use Mell\Bundle\SimpleDtoBundle\Model\DtoInterface;
 use Mell\Bundle\SimpleDtoBundle\Services\Dto\DtoExpandsManager;
 use Mell\Bundle\SimpleDtoBundle\Services\RequestManager\RequestManager;
@@ -50,6 +51,37 @@ class DtoExpandsListener
             return;
         }
 
-        $this->expandsManager->processExpands($dto, $expands, $this->dtoHelper->getDtoConfig());
+        $this->processDtoExpands($dto, $expands);
+    }
+
+    /**
+     * @param ApiEvent $apiEvent
+     */
+    public function onPostDtoCollectionEncode(ApiEvent $apiEvent)
+    {
+        $dto = $apiEvent->getData();
+        if ($apiEvent->getAction() !== ApiEvent::ACTION_CREATE_DTO_COLLECTION
+            || !$dto instanceof DtoCollectionInterface
+        ) {
+            return;
+        }
+
+        if (empty($this->requestManager->getExpands())) {
+            return;
+        }
+
+        foreach ($dto as $dtoItem) {
+            $this->processDtoExpands($dtoItem, $this->requestManager->getExpands());
+        }
+    }
+
+    /**
+     * @param $dto
+     * @param $expands
+     * @return DtoInterface
+     */
+    private function processDtoExpands($dto, $expands)
+    {
+        return $this->expandsManager->processExpands($dto, $expands, $this->dtoHelper->getDtoConfig());
     }
 }
