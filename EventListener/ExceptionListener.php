151193dd3d4b6aea48cd53a77c99d9ec76b565bb
Evgeniy Malykhin <e.malykhin@richmedia.systems>
2017-01-03 09:14:13 +0200

    log critical errors in ExceptionListener

--- EventListener/ExceptionListener.php
+++ EventListener/ExceptionListener.php
@@ -2,6 +2,7 @@
 
 namespace Mell\Bundle\SimpleDtoBundle\EventListener;
 
+use Psr\Log\LoggerInterface;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpKernel\Event\GetResponseForExceptionEvent;
@@ -16,7 +17,7 @@ class ExceptionListener
     const ENV_DEV = 'dev';
     const ENV_PROD = 'prod';
     const ENV_TEST = 'test';
-    const UNKNOWN_ERROR_MESSAGE = 'Unknown error';
+    const SERVER_ERROR_MESSAGE = 'Server error';
 
     /** @var array */
     protected $exceptionCodeMessageMap = [
@@ -26,6 +27,8 @@ class ExceptionListener
         Response::HTTP_METHOD_NOT_ALLOWED => 'Method not allowed',
         Response::HTTP_INTERNAL_SERVER_ERROR => 'Server error',
     ];
+    /** @var LoggerInterface */
+    protected $logger;
     /** @var string */
     protected $environment;
     /** @var bool */
@@ -33,11 +36,13 @@ class ExceptionListener
 
     /**
      * ExceptionListener constructor.
-     * @param string $environment
-     * @param bool $debug
+     * @param LoggerInterface $logger
+     * @param param string $environment
+     * @param param bool $debug
      */
-    public function __construct($environment, $debug)
+    public function __construct(LoggerInterface $logger, $environment, $debug)
     {
+        $this->logger = $logger;
         $this->environment = $environment;
         $this->debug = $debug;
     }
@@ -48,13 +53,15 @@ class ExceptionListener
     public function onKernelException(GetResponseForExceptionEvent $event)
     {
         $exception = $event->getException();
-        $code = $exception instanceof HttpException ? $exception->getStatusCode() : $exception->getCode();
+        $code = $exception instanceof HttpException
+            ? $exception->getStatusCode()
+            : Response::HTTP_INTERNAL_SERVER_ERROR;
         $data = [];
         if ($this->getEnvironment() === self::ENV_PROD) {
             if (isset($this->exceptionCodeMessageMap[$code])) {
                 $data['_error'] = $this->exceptionCodeMessageMap[$code];
             } else {
-                $data['_error'] = self::UNKNOWN_ERROR_MESSAGE;
+                $data['_error'] = self::SERVER_ERROR_MESSAGE;
             }
         } else {
             $data['_error'] = $exception->getMessage();
@@ -63,6 +70,10 @@ class ExceptionListener
                 $data['_line'] = $exception->getLine();
                 $data['_trace'] = $exception->getTraceAsString();
             }
+        }
+
+        if ($code === Response::HTTP_INTERNAL_SERVER_ERROR) {
+            $this->logger->critical($exception->getMessage());
         }
 
         $event->setResponse(new JsonResponse($data, $code ? : JsonResponse::HTTP_INTERNAL_SERVER_ERROR));
