8174c28ac5357c0bc5b420040f18a2ef06c76103
Viktor Konovalov <konovalov.viktor@richmedia.systems>
2017-12-18 17:32:22 +0200

    return for CrudManager DTOs always instead of entities

--- Services/Crud/CrudManager.php
+++ Services/Crud/CrudManager.php
@@ -101,7 +101,7 @@ class CrudManager
         $event = new ApiEvent($entity, ApiEvent::ACTION_CREATE);
         $this->eventDispatcher->dispatch(ApiEvent::EVENT_POST_FLUSH, $event);
 
-        return $entity;
+        return $this->dtoManager->createDto($entity, DtoInterface::DTO_GROUP_READ, $this->requestManager->getFields());
     }
 
     /**
@@ -149,7 +149,7 @@ class CrudManager
         $event = new ApiEvent($entity, ApiEvent::ACTION_UPDATE);
         $this->eventDispatcher->dispatch(ApiEvent::EVENT_POST_FLUSH, $event);
 
-        return $entity;
+        return $this->dtoManager->createDto($entity, DtoInterface::DTO_GROUP_READ, $this->requestManager->getFields());
     }
 
     /**
